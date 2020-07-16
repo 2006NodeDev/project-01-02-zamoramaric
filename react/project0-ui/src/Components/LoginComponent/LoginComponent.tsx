5350130b4d9c1571774ae34dbd391aecfc3ba817
zamoramaric <66961034+zamoramaric@users.noreply.github.com>
2020-07-16 04:46:58 -0400

    Sign In UI Layout completed

--- react/project0-ui/src/Components/LoginComponent/LoginComponent.tsx
+++ react/project0-ui/src/Components/LoginComponent/LoginComponent.tsx
@@ -4,25 +4,51 @@ import Button from '@material-ui/core/Button'
 import {SiteLogin} from '../../Remote/loginsite-app/SiteLogin'
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
+import { Typography, Container, CssBaseline } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
 
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  
+}));
 
 export const LoginComponent:FunctionComponent<any> = (props) => {
-    const [username, changeUsername] = useState('')  //2 bits of state from react
-    const [password, changePassword] = useState('') //one for username and one for password
+    
+  const classes = useStyles();
+
+  const [username, changeUsername] = useState('')  //2 bits of state from react
+  const [password, changePassword] = useState('') //one for username and one for password
 
 
  const updateUsername = (event:any) =>{
         event.preventDefault(
         changeUsername(event.currentTarget.value)
         )
-
     }
 
-    const updatePassword = (event:any) => {
+ const updatePassword = (event:any) => {
         event.preventDefault()
         changePassword(event.currentTarget.value)
     }
-    const loginSubmit = (e:SyntheticEvent)=>{
+ const loginSubmit = (e:SyntheticEvent)=>{
         e.preventDefault()
         SiteLogin(username, password)
         changePassword('')
@@ -31,22 +57,32 @@ export const LoginComponent:FunctionComponent<any> = (props) => {
     
 //autoComplete="off" onSubmit={loginSubmit}
     return (
-        <div>
-            {/* by default the submit event in a form tries to send a get request to the href value in the form */}
-            <form autoComplete="off" onSubmit={loginSubmit}>
-                <TextField id="standard-basic" type='username' label="Username" value = {username} onChange = {updateUsername} />
-                <TextField id="standard-basic" type='password' label="Password" value = {password} onChange = {updatePassword} />
-                <Button type='submit' variant="contained" color="primary">Login</Button>
 
-              <Grid container>
-            <Grid item>
+      <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+          {/* by default the submit event in a form tries to send a get request to the href value in the form */}
+        
+          <Typography component="h1" variant="h5">
+           Sign in
+          </Typography>
+          <form autoComplete="off" onSubmit={loginSubmit} className={classes.form} noValidate>
+              <TextField variant="outlined" margin="normal" required fullWidth id="standard-basic" type='username' label="Username" value = {username} onChange = {updateUsername} />
+              <TextField variant="outlined" margin="normal" required fullWidth id="standard-basic" type='password' label="Password" value = {password} onChange = {updatePassword} />
+              <Button type='submit' fullWidth variant="contained" color="primary" className={classes.submit}> Login </Button>
+         
+          <Grid container>
+
+          <Grid item>
               <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
+              {"Don't have an account? Sign Up"}
               </Link>
-            </Grid>
           </Grid>
-            </form>
-        </div>
+
+          </Grid>
+          </form>
+      </div>
+      </Container>
     )
 
 
